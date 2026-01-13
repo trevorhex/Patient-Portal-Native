65dce04e0cec5a7480bf6fc3df16394054945198
Trevor Smith <trevorsmith@onetable.org>
2026-01-13 10:29:27 -0800

    persist user store

--- stores/user.ts
+++ stores/user.ts
@@ -1,11 +1,16 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
 export type UserState = {
   authToken: string | null
   setAuthToken: (token: string | null) => void
 }
 
-export const useUserStore = create<UserState>((set) => ({
+export const useUserStore = create(persist<UserState>((set) => ({
   authToken: null,
   setAuthToken: (token) => set({ authToken: token }),
+}), {
+  name: 'user-storage',
+  storage: createJSONStorage(() => AsyncStorage)
 }))
