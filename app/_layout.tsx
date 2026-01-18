efad01f4b0341a70886f28f4bc3ef9c6c9f00e54
Trevor Smith <trevorsmith@onetable.org>
2026-01-18 10:06:04 -0800

    refactor newIssue screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import { Stack } from 'expo-router/stack'
+import { useRouter } from 'expo-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { XIcon } from 'lucide-react-native'
+import { IconButton } from '@/components/IconButton'
 
 const queryClient = new QueryClient()
 
 export default function RootLayout() {
+  const router = useRouter()
   return (
     <QueryClientProvider client={queryClient}>
       <Stack screenOptions={{ headerShown: false, animation: 'none' }}>
@@ -11,8 +15,16 @@ export default function RootLayout() {
         <Stack.Screen name="(portal)" />
         <Stack.Screen name="(issues)/newIssue" options={{
           presentation: 'modal',
-          title: 'New Item',
-          animation: 'slide_from_bottom'
+          title: 'New Issue',
+          animation: 'slide_from_bottom',
+          headerShown: true,
+          headerTransparent: true,
+          headerTintColor: '#fff',
+          headerRight: () => <IconButton
+            onPress={router.back}
+            accessibilityLabel="Add New Issue"
+            Icon={XIcon}
+          />,
         }} />
       </Stack>
     </QueryClientProvider>
